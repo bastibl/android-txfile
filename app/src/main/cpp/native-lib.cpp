4515f5efaba54c08883b07afe206a05cfc0dbffc
Bastian Bloessl <mail@bastibl.net>
2021-06-29 09:29:46 +0200

    update build/deps and start implementation

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -5,35 +5,16 @@
 
 #include <gnuradio/logger.h>
 #include <gnuradio/top_block.h>
-#include <gnuradio/blocks/ctrlport_probe2_f.h>
-#include <gnuradio/uhd/usrp_source.h>
-#include <gnuradio/blocks/sub.h>
-#include <gnuradio/filter/fir_filter_blk.h>
-#include <gnuradio/filter/single_pole_iir_filter_ff.h>
-#include <gnuradio/analog/quadrature_demod_cf.h>
-#include <gnuradio/blocks/ctrlport_probe2_f.h>
-#include <gnuradio/blocks/message_debug.h>
-#include <gnuradio/blocks/delay.h>
-#include <gnuradio/blocks/complex_to_mag_squared.h>
-#include <gnuradio/blocks/complex_to_mag.h>
-#include <gnuradio/blocks/multiply_conjugate_cc.h>
-#include <gnuradio/blocks/moving_average.h>
-#include <gnuradio/blocks/divide.h>
-#include <gnuradio/blocks/stream_to_vector.h>
-#include <gnuradio/blocks/pdu_to_tagged_stream.h>
+#include <gnuradio/uhd/usrp_sink.h>
+#include <gnuradio/analog/sig_source.h>
+#include <gnuradio/blocks/file_source.h>
+#include <gnuradio/blocks/multiply.h>
 #include <gnuradio/blocks/null_sink.h>
-#include <gnuradio/blocks/ctrlport_probe2_c.h>
-#include <gnuradio/fft/fft_vcc.h>
-#include <ieee802_11/sync_short.h>
-#include <ieee802_11/sync_long.h>
-#include <ieee802_11/frame_equalizer.h>
-#include <ieee802_11/decode_mac.h>
-#include <ieee802_11/parse_mac.h>
-#include <gnuradio/zeromq/pub_msg_sink.h>
+#include <gnuradio/blocks/throttle.h>
+#include <gnuradio/filter/rational_resampler_base.h>
 #include <stdlib.h>
 
 gr::top_block_sptr tb;
-gr::uhd::usrp_source::sptr src;
 
 extern "C"
 JNIEXPORT jobject JNICALL
@@ -44,7 +25,7 @@ Java_net_bastibl_wlan_MainActivity_fgInit(JNIEnv * env, jobject /*this*/, int fd
 
     const char *usbfs_path = env->GetStringUTFChars(usbfsPath, NULL);
 
-    tb = gr::make_top_block("fg");
+    tb = gr::make_top_block("txfile");
 
     std::stringstream args;
     args << "bbl=foo,type=b200,fd=" << fd << ",usbfs_path=" << usbfs_path;
@@ -54,55 +35,30 @@ Java_net_bastibl_wlan_MainActivity_fgInit(JNIEnv * env, jobject /*this*/, int fd
     stream_args.cpu_format = "fc32";
     stream_args.otw_format = "sc16";
 
-    int window_size = 48;
-    int sync_length = 320;
+    double sample_rate = 100e6/100;
+    int interpolation_factor = 20;
+    int LO_offset = int(100e3);
+    //double center_frequency = 500e6;
 
-    // Declare our GNU Radio blocks
-    src = gr::uhd::usrp_source::make(args.str(), stream_args);
-    src->set_samp_rate(20e6);
-    src->set_center_freq(uhd::tune_request_t(5.18e9, 0));
-    src->set_normalized_gain(0.7);
+    std::vector<gr_complex> taps;
 
-    gr::blocks::delay::sptr adelay = gr::blocks::delay::make(sizeof(gr_complex), 16);
-    gr::blocks::multiply_conjugate_cc::sptr mconj = gr::blocks::multiply_conjugate_cc::make(1);
-    gr::blocks::complex_to_mag_squared::sptr cm2 = gr::blocks::complex_to_mag_squared::make(1);
-    gr::blocks::moving_average_ff::sptr maff = gr::blocks::moving_average_ff::make(window_size+16, 1, 4000, 1);
-    gr::blocks::moving_average_cc::sptr macc = gr::blocks::moving_average_cc::make(window_size, 1, 4000, 1);
-    gr::blocks::complex_to_mag::sptr cm = gr::blocks::complex_to_mag::make(1);
-    gr::blocks::divide_ff::sptr divide = gr::blocks::divide_ff::make(1);
-    gr::ieee802_11::sync_short::sptr sync_short = gr::ieee802_11::sync_short::make(0.56, 2, false, false);
-    gr::ieee802_11::sync_long::sptr sync_long = gr::ieee802_11::sync_long::make(sync_length, false, false);
-    gr::blocks::delay::sptr ldelay = gr::blocks::delay::make(sizeof(gr_complex), sync_length);
-    gr::blocks::stream_to_vector::sptr s2v = gr::blocks::stream_to_vector::make(sizeof(gr_complex), 64);
-    gr::fft::fft_vcc::sptr fft = gr::fft::fft_vcc::make(64, true, std::vector<float>(64, 1.0), true, 1);
-    gr::ieee802_11::frame_equalizer::sptr equal = gr::ieee802_11::frame_equalizer::make(LS, 5.89e9, 5e6, false, false);
-    gr::blocks::pdu_to_tagged_stream::sptr pdu2ts = gr::blocks::pdu_to_tagged_stream::make(gr::blocks::pdu::complex_t, "packet_len");
-    gr::blocks::ctrlport_probe2_c::sptr probe = gr::blocks::ctrlport_probe2_c::make("const", "foo", 240, 0);
-    gr::ieee802_11::decode_mac::sptr decode = gr::ieee802_11::decode_mac::make(false, false);
-    gr::zeromq::pub_msg_sink::sptr zmq = gr::zeromq::pub_msg_sink::make("tcp://127.0.0.1:5503", -1);
+    //gr::uhd::usrp_sink::sptr snk = gr::uhd::usrp_sink::make(args.str(), stream_args);
+    //snk->set_samp_rate(20e6);
+    //snk->set_center_freq(uhd::tune_request_t(5.18e9, 0));
+    //snk->set_normalized_gain(0.7);
+    gr::filter::rational_resampler_base_ccc::sptr rational_resampler = gr::filter::rational_resampler_base_ccc::make(interpolation_factor, 1, taps);
+    gr::blocks::throttle::sptr throttle = gr::blocks::throttle::make(sizeof(gr_complex), 1e6, true);
+    gr::blocks::null_sink::sptr null_sink = gr::blocks::null_sink::make(sizeof(gr_complex));
+    gr::blocks::multiply_cc::sptr multiply = gr::blocks::multiply_cc::make(1);
+    gr::blocks::file_source::sptr file_source = gr::blocks::file_source::make(sizeof(gr_complex), "/home/basti/Downloads/ucssTxSig_UserID1_ModCod5_SF100_osf4.mat.bin", true, 0, 0);
+    gr::analog::sig_source_c::sptr sig_source = gr::analog::sig_source_c::make(sample_rate, gr::analog::GR_COS_WAVE, LO_offset, .7, 0,0);
 
-    tb->connect(src, 0, adelay, 0);
-    tb->connect(src, 0, cm2, 0);
-    tb->connect(src, 0, mconj, 0);
-    tb->connect(adelay, 0, mconj, 1);
-    tb->connect(adelay, 0, sync_short, 0);
-    tb->connect(mconj, 0, macc, 0);
-    tb->connect(macc, 0, cm, 0);
-    tb->connect(macc, 0, sync_short, 1);
-    tb->connect(cm, 0, divide, 0);
-    tb->connect(cm2, 0, maff, 0);
-    tb->connect(maff, 0, divide, 1);
-    tb->connect(divide, 0, sync_short, 2);
-    tb->connect(sync_short, 0, sync_long, 0);
-    tb->connect(sync_short, 0, ldelay, 0);
-    tb->connect(ldelay, 0, sync_long, 1);
-    tb->connect(sync_long, 0, s2v, 0);
-    tb->connect(s2v, 0, fft, 0);
-    tb->connect(fft, 0, equal, 0);
-    tb->connect(equal, 0, decode, 0);
-    tb->msg_connect(equal, "symbols", pdu2ts, "pdus");
-    tb->connect(pdu2ts, 0, probe, 0);
-    tb->msg_connect(decode, "out", zmq, "in");
+    tb->connect(file_source, 0, throttle, 0);
+    tb->connect(throttle, 0, rational_resampler, 0);
+    tb->connect(rational_resampler, 0, multiply, 0);
+
+    tb->connect(sig_source, 0, multiply, 1);
+    tb->connect(multiply, 0, null_sink, 0);
 
     GR_DEBUG("gnuradio", "constructed flowgraph");
 
@@ -129,25 +85,6 @@ JNIEXPORT jobject JNICALL
 Java_net_bastibl_wlan_MainActivity_fgStop(JNIEnv * env, jobject /*this*/) {
     tb->stop();
     tb->wait();
-
-    return nullptr;
-}
-
-extern "C"
-JNIEXPORT jstring JNICALL
-Java_net_bastibl_wlan_MainActivity_fgRep(JNIEnv * env, jobject /*this*/) {
-
-    return env->NewStringUTF(tb->edge_list().c_str());
-}
-
-extern "C"
-JNIEXPORT jobject JNICALL
-Java_net_bastibl_wlan_MainActivity_setFreq(JNIEnv * env, jobject /*this*/, double freq) {
-
-    if(src) {
-        pmt::pmt_t command = pmt::cons(pmt::mp("freq"), pmt::mp(freq));
-        src->post(pmt::intern("command"), command);
-    }
 
     return nullptr;
 }
---
+++ app/src/main/java/net/bastibl/txfile/MainActivity.kt
@@ -0,0 +1,182 @@
+package net.bastibl.txfile
+
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.PendingIntent
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.content.pm.PackageManager
+import android.hardware.usb.UsbDevice
+import android.hardware.usb.UsbDeviceConnection
+import android.hardware.usb.UsbManager
+import android.os.Bundle
+import android.os.Environment
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import net.bastibl.txfile.databinding.ActivityMainBinding
+import java.io.*
+import java.util.*
+import kotlin.concurrent.thread
+
+private const val ACTION_USB_PERMISSION = "com.android.example.USB_PERMISSION"
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private val usbReceiver = object : BroadcastReceiver() {
+
+        @Suppress("IMPLICIT_CAST_TO_ANY")
+        override fun onReceive(context: Context, intent: Intent) {
+            if (ACTION_USB_PERMISSION == intent.action) {
+                synchronized(this) {
+                    val device: UsbDevice? = intent.getParcelableExtra(UsbManager.EXTRA_DEVICE)
+
+                    if (intent.getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false)) {
+                        device?.apply {
+                            setupUSB(device)
+                        }
+                    } else {
+                        Log.d("GR", "permission denied for device $device")
+                    }
+                }
+            }
+        }
+    }
+
+    private fun checkStoragePermission() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this,
+                arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                123)
+        } else {
+            checkHWPermission()
+        }
+    }
+
+    private fun checkHWPermission() {
+        val manager = getSystemService(Context.USB_SERVICE) as UsbManager
+        val deviceList: HashMap<String, UsbDevice> = manager.deviceList
+        deviceList.values.forEach { device ->
+            if(device.vendorId == 0x2500) {
+                val permissionIntent = PendingIntent.getBroadcast(this, 0, Intent(ACTION_USB_PERMISSION), 0)
+                val filter = IntentFilter(ACTION_USB_PERMISSION)
+                registerReceiver(usbReceiver, filter)
+
+                manager.requestPermission(device, permissionIntent)
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            123 -> {
+                checkHWPermission()
+            }
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        @Suppress("DEPRECATION")
+        val external = Environment.getExternalStorageDirectory()
+        val uhdDir = File(external.toString() + File.separator + "uhd")
+
+        if (!uhdDir.exists()) {
+            Log.d("uhd", "UHD directory does not exist, creating.")
+            uhdDir.mkdirs()
+        }
+
+        val assetManager = assets
+        try {
+            val files = assetManager.list("uhd")
+            for (f in files!!) {
+                Log.d("uhd", "UHD asset file: $f")
+            }
+            for (f in files) {
+                Log.d("uhd", "Copying file:$f")
+                val `in` = assetManager.open("uhd" + File.separator + f)
+                val out: OutputStream = FileOutputStream(uhdDir.toString() + File.separator + f)
+                copyFile(`in`, out)
+                `in`.close()
+                out.flush()
+                out.close()
+            }
+        } catch (e: IOException) {
+            Log.e("uhd", "Failed to copy asset file", e)
+        }
+
+
+
+
+        thread(start=true) {
+            while (!Thread.currentThread().isInterrupted) {
+                // runOnUiThread {
+                // }
+            }
+        }
+
+        checkStoragePermission()
+    }
+
+    @Throws(IOException::class)
+    private fun copyFile(`in`: InputStream, out: OutputStream) {
+        val buffer = ByteArray(1024)
+        var read: Int
+        while (`in`.read(buffer).also { read = it } != -1) {
+            out.write(buffer, 0, read)
+        }
+    }
+
+    @SuppressLint("SetTextI18n")
+    fun setupUSB(usbDevice: UsbDevice) {
+
+        val manager = getSystemService(Context.USB_SERVICE) as UsbManager
+        val connection: UsbDeviceConnection = manager.openDevice(usbDevice)
+
+        val fd = connection.fileDescriptor
+        val usbfsPath = usbDevice.deviceName
+
+        val vid = usbDevice.vendorId
+        val pid = usbDevice.productId
+
+        Log.d("gnuradio", "#################### NEW RUN ###################")
+        Log.d("gnuradio", "Found fd: $fd  usbfs_path: $usbfsPath")
+        Log.d("gnuradio", "Found vid: $vid  pid: $pid")
+
+        binding.sampleText.text =
+            "Found fd: $fd  usbfsPath: $usbfsPath vid: $vid  pid: $pid"
+
+        thread(start = true, priority = Thread.MAX_PRIORITY) {
+            fgInit(fd, usbfsPath)
+            fgStart(cacheDir.absolutePath)
+        }
+    }
+
+    override fun onStop() {
+        fgStop()
+        super.onStop()
+    }
+
+    private external fun fgInit(fd: Int, usbfsPath: String): Void
+    private external fun fgStart(tmpName: String): Void
+    private external fun fgStop(): Void
+
+    companion object {
+        init {
+            System.loadLibrary("native-lib")
+        }
+    }
+}
